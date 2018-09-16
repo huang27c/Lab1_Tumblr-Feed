c26465254599d49748db035126b7347ea1d53d8c
Ching Ching Huang <chingchinghuang@n225-250.mtholyoke.edu>
2018-09-16 18:43:36 -0400

    Update files

---
+++ Lab1_Tumblr-Feed/ViewControllers/PhotosViewController.swift
@@ -0,0 +1,70 @@
+//
+//  PhotosViewController.swift
+//  Lab1_Tumblr-Feed
+//
+//  Created by Ching Ching Huang on 9/13/18.
+//  Copyright © 2018 Ching Ching Huang. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+
+    @IBOutlet weak var phototableview: UITableView!
+    
+    var posts: [[String: Any]] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        phototableview.delegate = self
+        phototableview.dataSource = self
+        
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                print(dataDictionary)
+                // TODO: Get the posts and store in posts property
+                
+                // Get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                // Store the returned array of dictionaries in our posts property
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+                
+                // TODO: Reload the table view
+            }
+        }
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "This is row \(indexPath.row)"
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "photocell") as! TableViewCell
+        
+        // Configure YourCustomCell using the outlets that you've defined.
+        
+        return cell
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Lab1_Tumblr-Feed' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Lab1_Tumblr-Feed
+
+
+
+  target 'Lab1_Tumblr-FeedTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Lab1_Tumblr-FeedUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
